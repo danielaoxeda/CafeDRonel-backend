cacb55c76d05248b548fcb56aa42aa16f77f6e16
rodrigomaldonadov <rodrigomaldonadov8@gmail.com>
2025-09-27 22:00:05 -0500

    refactor: update imports and annotations, add JWT secret property
    - Refactored DetallePedidoRepository for updated model import.
    - Added `@Data` annotation and new `rol` field to Usuario entity.
    - Introduced JWT secret configuration in application.properties.

--- src/main/java/com/cafedronel/cafedronelbackend/data/model/Usuario.java
+++ src/main/java/com/cafedronel/cafedronelbackend/data/model/Usuario.java
@@ -1,27 +1,31 @@
 package com.cafedronel.cafedronelbackend.data.model;
 
 import jakarta.persistence.*;
+import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import com.cafedronel.cafedronelbackend.model.Rol;
 
 import java.util.List;
 
 @Entity
 @Table(name = "usuario")
-@Getter @Setter @NoArgsConstructor
+@Data
 public class Usuario {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer idUsuario;
 
     private String nombre;
-
-    @Column(unique = true)
     private String correo;
     private String contrasena;
     private String telefono;
     private String direccion;
+
+    @Enumerated(EnumType.STRING)
+    private Rol rol;
 
     @OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL)
     private List<Pedido> pedidos;
--- src/main/java/com/cafedronel/cafedronelbackend/repository/DetallePedidoRepository.java
+++ src/main/java/com/cafedronel/cafedronelbackend/repository/DetallePedidoRepository.java
@@ -1,6 +1,6 @@
 package com.cafedronel.cafedronelbackend.repository;
 
-import com.cafedronel.cafedronelbackend.data.model.DetallePedido;
+import com.cafedronel.cafedronelbackend.model.DetallePedido;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DetallePedidoRepository extends JpaRepository<DetallePedido, Integer> {
