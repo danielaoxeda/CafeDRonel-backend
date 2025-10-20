440d427b358fc8f60be2e4306518e5e12f4c51ad
rodrigomaldonadov <rodrigomaldonadov8@gmail.com>
2025-10-20 13:57:57 -0500

    Añadir configuración de Swagger con soporte para JWT

---
+++ src/main/java/com/cafedronel/cafedronelbackend/config/OpenApiConfig.java
@@ -0,0 +1,44 @@
+package com.cafedronel.cafedronelbackend.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "API de Café D'Ronel",
+                version = "1.0",
+                description = "API REST para la aplicación de Café D'Ronel",
+                contact = @Contact(
+                        name = "Café D'Ronel",
+                        email = "info@cafedronel.com"
+                )
+        ),
+        servers = {
+                @Server(
+                        description = "Local ENV",
+                        url = "http://localhost:8080"
+                )
+        },
+        security = {
+                @SecurityRequirement(name = "bearerAuth")
+        }
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        description = "JWT auth description",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
+public class OpenApiConfig {
+    // La configuración se realiza mediante anotaciones
+}
