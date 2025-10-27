2edc7eed132df4245e37c655af8760ec4a215671
Rodrigo Maldonado Vasquez <rodrigomaldonadov8@gmail.com>
2025-10-27 18:54:12 -0500

    refactor: update API endpoints and improve test cases for authentication and order management

--- src/main/java/com/cafedronel/cafedronelbackend/data/dto/auth/AuthResponse.java
+++ src/main/java/com/cafedronel/cafedronelbackend/data/dto/auth/AuthResponse.java
@@ -1,8 +1,10 @@
 package com.cafedronel.cafedronelbackend.data.dto.auth;
 
 
-public record AuthResponse (
-    String token,
-    String email,
-    String rol
-) {}
+public record AuthResponse(
+        String token,
+        String email,
+        String rol,
+        Integer id
+) {
+}
